37e92f4b321c5fbe13ad56329d6af51abab9863a
Paul Stevens <twinkisan@hotmail.com>
2020-09-30 14:52:26 -0700

    replaces label/img funcationality for radio buttons

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 // import functions and grab DOM elements
-
+// grabbing HTML elements
+const computerDisplay = document.getElementById('computer-display');
+const winDisplay = document.getElementById('wins');
+const lossDisplay = document.getElementById('losses');
+const drawDisplay = document.getElementById('draws');
+const infoDisplay = document.getElementById('info-display')
+const playButton = document.getElementById('play-button');
 // initialize state
+// establishing state
+let userWins = 0;
+let userLoss = 0;
+let userDraws = 0;
+let computerThrow = '';
+let userThrow = '';
 
+playButton.addEventListener('click', () => {
+    console.log(document.querySelector(':checked'));
+})
+
+
+//const checkedRadioButton = document.querySelector(':checked')
 // set event listeners to update state and DOM
